e044913e7cd68bc5677620c5a7a3c2a6eb8874be
Valentin Besse <valentinbesse@aumbox.net>
2022-09-09 17:30:54 +0200

    MenuItem

--- Abby/Abby.DataAccess/Data/ApplicationDbContext.cs
+++ Abby/Abby.DataAccess/Data/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ namespace Abby.DataAccess.Data
 
         public DbSet<Category> Category { get; set; }
         public DbSet<FoodType> FoodType { get; set; }
+        public DbSet<MenuItem> MenuItem { get; set; }
+
 
 
     }
--- Abby/Abby.DataAccess/Repository/FoodTypeRepository.cs
+++ Abby/Abby.DataAccess/Repository/FoodTypeRepository.cs
@@ -10,19 +10,28 @@ using System.Threading.Tasks;
 
 namespace Abby.DataAccess.Repository
 {
-    public class FoodTypeRepository : Repository<FoodType>, IFoodTypeRepository
+    public class MenuItemRepository : Repository<MenuItem>, IMenuItemRepository
     {
         private readonly ApplicationDbContext _db;
 
-        public FoodTypeRepository(ApplicationDbContext db) : base(db)
+        public MenuItemRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
         }
 
-        public void Update(FoodType foodType)
+        public void Update(MenuItem obj)
         {
-            var objFromDb = _db.FoodType.FirstOrDefault(u => u.Id == foodType.Id);
-            objFromDb.Name = foodType.Name;
+            var objFromDb = _db.MenuItem.FirstOrDefault(u => u.Id == obj.Id);
+            objFromDb.Name = obj.Name;
+            objFromDb.Description = obj.Description;
+            objFromDb.Price = obj.Price;
+            objFromDb.CategoryId = obj.CategoryId;
+            objFromDb.FoodTypeId = obj.FoodTypeId;
+            if (objFromDb.Image != null)
+            {
+                objFromDb.Image = obj.Image;
+            }
+
         }
     }
 }
